db4b3f7601dacccf34b2d48c737e13e14864d4fd
ttphuong172 <ttphuong172@gmail.com>
2020-12-07 06:09:59 +0700

    hoan thien bai doi tien te

--- Module 1/7. Bien, kieu du lieu va toan tu/Chuyen doi tien te/main.js
+++ Module 1/7. Bien, kieu du lieu va toan tu/Chuyen doi tien te/main.js
@@ -1,6 +1,26 @@
-
+document.getElementById('btnconvert').addEventListener('click',convert)
 function convert() {
     var amount = document.getElementById('amount').value;
-    var result = parseFloat(amount)/23000;
-    document.getElementById('result').innerHTML=result;
+    var fromCurrency = document.getElementById('fromcurrency').value;
+    var toCurrency = document.getElementById('tocurrency').value;
+    var denominationsUSD=23000;
+    var result;
+     if (fromCurrency === 'vietnam'){
+        if (toCurrency==='vietnam'){
+            result = 'Vui long chon lai'
+        }
+        else{
+            result = parseFloat(amount) / denominationsUSD;
+        }
+    }
+    else if (fromCurrency==='usd'){
+        if (toCurrency==='usd'){
+            result = 'Vui long chon lai'
+        }
+        else{
+            result = parseFloat(amount) * denominationsUSD;
+        }
+    }
+
+    document.getElementById('result').innerHTML = result;
 }
