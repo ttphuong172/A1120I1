cf3dcc3d3fd995d1e44ceeeae89fff44ed973dd5
ttphuong172 <ttphuong172@gmail.com>
2020-12-12 11:54:50 +0700

    commemt while loop

--- Module 1/10. Cau truc lap/Bai tap luyen tap 1/Tim so dau tien trong day Fibonacci chia het cho 1 so/main.js
+++ Module 1/10. Cau truc lap/Bai tap luyen tap 1/Tim so dau tien trong day Fibonacci chia het cho 1 so/main.js
@@ -7,9 +7,11 @@ function fiboMod(){
     nextNumber=2;
     var mod=parseInt(document.getElementById('mod').value);
     while (nextNumber%mod){
+        //Dung thi thuc hien vong lap: gia tri khac 0 se la dung
         var nextNumber=firstNumber+secondNumber;
         firstNumber=secondNumber;
         secondNumber=nextNumber;
     }
+        //Gia tri bang 0 thi se sai va thuc hien lenh ben duoi
     document.getElementById('result').innerHTML=nextNumber;
 }
