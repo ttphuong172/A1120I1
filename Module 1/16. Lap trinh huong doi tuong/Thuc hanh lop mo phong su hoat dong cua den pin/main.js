db1a195f84f015e7c8ece8ef637cff607944f1c3
ttphuong172 <ttphuong172@gmail.com>
2021-01-07 16:25:22 +0700

    bai tap den pim

---
+++ Module 1/16. Lap trinh huong doi tuong/Thuc hanh lop mo phong su hoat dong cua den pin/main.js
@@ -0,0 +1,55 @@
+class Battery{
+  constructor(energy){
+    this.energy=energy;
+  }
+  getEnergy(){
+    return this.energy
+  }
+  setEnergy(energy){
+    this.energy=energy;
+  }
+  decreaseEnergy(){
+    if(this.energy>0){
+      this.energy--
+    }
+  }
+}
+
+class FlashLamp{
+  constructor(status,battery){
+    this.status=status;
+    this.battery=battery;
+  }
+  setBattery(battery){
+    this.battery=Battery.battery;
+  }
+
+  getBatteryInfo(){
+    return this.battery.getEnergy();
+  }
+
+  light(){
+    if (this.status){
+      alert('Lighting');
+    } else{
+      alert('Not lighting')
+    }
+  }
+  turnOn(){
+    this.status=true
+  }
+  turnOff(){
+    this.status=false
+  }
+}
+
+let battery=new Battery();
+battery.setEnergy(10);
+let flashLamp=new FlashLamp;
+flashLamp.setBattery(battery);
+
+
+console.log(battery.getEnergy())
+console.log(flashLamp.getBatteryInfo())
+flashLamp.turnOn();
+flashLamp.light();
