5dcba2f2b6c916659e81c44a3ab6bd90e901da34
ttphuong172 <ttphuong172@gmail.com>
2021-02-16 09:47:11 +0700

    Module 2

---
+++ Module2/src/IntroductiontoJava/BMI.java
@@ -0,0 +1,24 @@
+package IntroductiontoJava;
+import java.util.Scanner;
+public class BMI {
+    public static void main(String[] args) {
+        double bmi;
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Nhập vào cân nặng (theo kg)");
+        double weigth=scanner.nextDouble();
+        System.out.println("Nhập vào chiều cao (theo mét)");
+        double height=scanner.nextDouble();
+        bmi=weigth/(Math.pow(height,2));
+        System.out.println("BMI: "+bmi);
+        if (bmi>=30){
+            System.out.println("Obese");
+        } else if(bmi>=25){
+            System.out.println("Overweight");
+        } else if(bmi>=18.5){
+            System.out.println("Normal");
+        } else{
+            System.out.println("Under weight");
+        }
+    }
+    }
+
---
+++ Module2/src/IntroductiontoJava/ReadNumber.java
@@ -0,0 +1,230 @@
+package IntroductiontoJava;
+import java.util.Scanner;
+
+public class ReadNumber {
+    public static void main(String[] args) {
+        String donvi = "";
+        String chuc = "";
+        String tram = "";
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhap vao so can doc:");
+        int number = scanner.nextInt();
+        int hangdonvi = number % 100;
+        int hangchuc = number%10;
+        int hangtram=number/100;
+        if (number < 10) {
+            switch (hangdonvi) {
+                case 1:
+                    donvi = "one";
+                    break;
+                case 2:
+                    donvi = "two";
+                    break;
+                case 3:
+                    donvi = "three";
+                    break;
+                case 4:
+                    donvi = "four";
+                    break;
+                case 5:
+                    donvi = "five";
+                    break;
+                case 6:
+                    donvi = "six";
+                    break;
+                case 7:
+                    donvi = "seven";
+                    break;
+                case 8:
+                    donvi = "eight";
+                    break;
+                case 9:
+                    donvi = "nine";
+                    break;
+            }
+            System.out.println(chuc + donvi);
+        } else if (number < 16) {
+            switch (number) {
+                case 10:
+                    chuc = "ten";
+                    break;
+                case 11:
+                    chuc = "eleven";
+                    break;
+                case 12:
+                    chuc = "twele";
+                case 13:
+                    chuc = "thirteen";
+                case 14:
+                    chuc = "fourteen";
+                case 15:
+                    chuc = "fifteen";
+
+            }
+            System.out.println(chuc + donvi);
+        } else if (number < 20) {
+            switch (hangdonvi) {
+                case 1:
+                    donvi = "one";
+                    break;
+                case 2:
+                    donvi = "two";
+                    break;
+                case 3:
+                    donvi = "three";
+                    break;
+                case 4:
+                    donvi = "four";
+                    break;
+                case 5:
+                    donvi = "five";
+                    break;
+                case 6:
+                    donvi = "six";
+                    break;
+                case 7:
+                    donvi = "seven";
+                    break;
+                case 8:
+                    donvi = "eight";
+                    break;
+                case 9:
+                    donvi = "nine";
+                    break;
+            }
+            switch (hangchuc) {
+                case 1:
+                    chuc = "teen";
+            }
+            System.out.println(donvi + chuc);
+        } else if (number < 100) {
+            switch (hangdonvi) {
+                case 1:
+                    donvi = "one";
+                    break;
+                case 2:
+                    donvi = "two";
+                    break;
+                case 3:
+                    donvi = "three";
+                    break;
+                case 4:
+                    donvi = "four";
+                    break;
+                case 5:
+                    donvi = "five";
+                    break;
+                case 6:
+                    donvi = "six";
+                    break;
+                case 7:
+                    donvi = "seven";
+                    break;
+                case 8:
+                    donvi = "eight";
+                    break;
+                case 9:
+                    donvi = "nine";
+                    break;
+            }
+            switch (hangchuc) {
+                case 1:
+                    chuc = "teen";
+                    break;
+                case 2:
+                    chuc = "twenty";
+                    break;
+                case 3:
+                    chuc = "thirty";
+                    break;
+                case 4:
+                    chuc = "forty";
+                    break;
+                case 5:
+                    chuc = "fifty";
+                    break;
+                case 6:
+                    chuc = "sixty";
+                    break;
+                case 7:
+                    chuc = "seventy";
+                    break;
+                case 8:
+                    chuc = "eighty";
+                    break;
+                case 9:
+                    chuc = "ninety";
+                    break;
+
+            }
+            System.out.println(chuc + donvi);
+        } else if (number<1000){
+            switch (hangdonvi) {
+                case 1:
+                    donvi = "one";
+                    break;
+                case 2:
+                    donvi = "two";
+                    break;
+                case 3:
+                    donvi = "three";
+                    break;
+                case 4:
+                    donvi = "four";
+                    break;
+                case 5:
+                    donvi = "five";
+                    break;
+                case 6:
+                    donvi = "six";
+                    break;
+                case 7:
+                    donvi = "seven";
+                    break;
+                case 8:
+                    donvi = "eight";
+                    break;
+                case 9:
+                    donvi = "nine";
+                    break;
+            }
+            switch (hangchuc) {
+                case 1:
+                    chuc = "teen";
+                    break;
+                case 2:
+                    chuc = "twenty";
+                    break;
+                case 3:
+                    chuc = "thirty";
+                    break;
+                case 4:
+                    chuc = "forty";
+                    break;
+                case 5:
+                    chuc = "fifty";
+                    break;
+                case 6:
+                    chuc = "sixty";
+                    break;
+                case 7:
+                    chuc = "seventy";
+                    break;
+                case 8:
+                    chuc = "eighty";
+                    break;
+                case 9:
+                    chuc = "ninety";
+                    break;
+
+            }
+            switch (hangtram){
+                case 1:
+                    tram="one hundred";
+            }
+            System.out.println(tram+chuc + donvi);
+        }
+
+    }
+
+}
