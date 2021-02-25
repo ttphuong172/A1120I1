c052aed1eafbb7a23c8606d338f908309bb16cb9
Tran Thanh Phuong <ttphuong172@gmail.com>
2021-02-25 16:13:31 +0700

    mang

---
+++ Module2/src/_1_IntroductiontoJava/Calendar.java
@@ -0,0 +1,40 @@
+package _1_IntroductiontoJava;
+
+import java.util.Scanner;
+public class Calendar {
+    public static void main(String[] args) {
+        String dayInMonth;
+        Scanner scaner = new Scanner(System.in);
+        System.out.println("Nhap vao thang can kiem tra");
+        int month=scaner.nextInt();
+        switch (month){
+            case 2:
+                dayInMonth="28 or 29";
+                break;
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                dayInMonth="31";
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                dayInMonth="30";
+                break;
+            default:
+                dayInMonth="";
+                break;
+        }
+        if(!dayInMonth.equals("")){
+            System.out.println("Thang "+month+" co "+dayInMonth+" ngay");
+        } else{
+            System.out.println("Lam gi co thang "+month+" cha noi");
+        }
+
+    }
+}
---
+++ Module2/src/_2_Loop/TH/LaiSuatNganHang.java
@@ -0,0 +1,17 @@
+package _2_Loop.TH;
+import java.util.Scanner;
+public class LaiSuatNganHang {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhap vao so tien gui: ");
+        double sotien=scanner.nextDouble();
+        System.out.print("Nhap vao so thang gui: ");
+        int sothang=scanner.nextInt();
+        System.out.print("Nhap vao lai suat: ");
+        double laisuat=scanner.nextDouble();
+
+        double sotien_nhanduoc = sotien * (laisuat/100)/12 * sothang;
+
+        System.out.println("So tien nhan duoc: "+ sotien_nhanduoc);
+    }
+}
