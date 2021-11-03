7f6e5bb0c6eef7a65da3d885b1fea75be139d6bc
phuongtt <ttphuong172@gmail.com>
2021-11-03 13:07:59 +0700

    footer hacker new

--- Module5/2-Angular/angularcomponenttemplate/src/app/app.module.ts
+++ Module5/2-Angular/angularcomponenttemplate/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {FormsModule} from "@angular/forms";
 import { LikesComponent } from './likes/likes.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LikesComponent
+    LikesComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
